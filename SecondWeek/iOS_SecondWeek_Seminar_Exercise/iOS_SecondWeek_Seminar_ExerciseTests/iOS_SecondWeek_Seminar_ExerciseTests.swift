f25a54f8e0ef9f84e067219e48e20aff9435a2f1
seu11ee <yslee2504@naver.com>
2020-05-08 20:36:08 +0900

    2차 세미나 실습

---
+++ SecondWeek/iOS_SecondWeek_Seminar_Exercise/iOS_SecondWeek_Seminar_ExerciseTests/iOS_SecondWeek_Seminar_ExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_SecondWeek_Seminar_ExerciseTests.swift
+//  iOS_SecondWeek_Seminar_ExerciseTests
+//
+//  Created by 이예슬 on 4/25/20.
+//  Copyright © 2020 이예슬. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_SecondWeek_Seminar_Exercise
+
+class iOS_SecondWeek_Seminar_ExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
