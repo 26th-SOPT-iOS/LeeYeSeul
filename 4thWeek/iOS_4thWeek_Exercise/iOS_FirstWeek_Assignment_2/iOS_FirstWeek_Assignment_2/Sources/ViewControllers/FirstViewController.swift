7f31a3b1200222b504c7735b2956517361db5df4
seu11ee <yslee2504@naver.com>
2020-05-20 21:55:09 +0900

    4주차 실습

--- 4thWeek/iOS_4thWeek_Exercise/iOS_FirstWeek_Assignment_2/iOS_FirstWeek_Assignment_2/Sources/ViewControllers/FirstViewController.swift
+++ 4thWeek/iOS_4thWeek_Exercise/iOS_FirstWeek_Assignment_2/iOS_FirstWeek_Assignment_2/Sources/ViewControllers/FirstViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    @IBOutlet weak var logIn: UIButton!
+    
+    @IBOutlet weak var logIn: UIButton! //로그인버튼 outlet
     override func viewDidLoad() {
         super.viewDidLoad()
         logIn.layer.cornerRadius = 20
@@ -22,8 +22,8 @@ class FirstViewController: UIViewController {
         let labelTap = UITapGestureRecognizer(target:self,action: #selector(self.labelTapped(_:)))
         signUp.addGestureRecognizer(labelTap)
         
-       //transparent navigationbar
-    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        //transparent navigationbar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
@@ -32,36 +32,59 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var idTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
-        
+    
     @IBOutlet weak var signUp: UILabel!
     
     @IBAction func loginButton(_ sender: Any) {
-        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier:"customTabbarController") as? UITabBarController else{return}
-//        receiveViewController.id = idTextField.text
-//        receiveViewController.pw = passwordTextField.text
-        receiveViewController.modalPresentationStyle = .fullScreen
-        self.present(receiveViewController,animated:true,completion:nil)
-        idTextField.text = ""
-        passwordTextField.text = ""
-    
+        guard let inputID = idTextField.text else { return }
+        guard let inputPWD = passwordTextField.text else { return }
+        LoginService.shared.login(id: inputID, pwd: inputPWD) { networkResult in
+            switch networkResult {
+            case .success(let token):
+                guard let token = token as? String else { return }
+                UserDefaults.standard.set(token, forKey: "token")
+                guard let tabbarController = self.storyboard?.instantiateViewController(identifier:
+                    "customTabbarController") as? UITabBarController else { return }
+                tabbarController.modalPresentationStyle = .fullScreen
+                self.present(tabbarController, animated: true, completion: nil)
+            case .requestErr(let message):
+                guard let message = message as? String else { return }
+                let alertViewController = UIAlertController(title: "로그인 실패", message: message, preferredStyle: .alert)
+                let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+                alertViewController.addAction(action)
+                self.present(alertViewController, animated: true, completion: nil)
+            case .pathErr: print("path")
+            case .serverErr: print("serverErr")
+            case .networkFail: print("networkFail") }
+        }
+        
+        
+        //        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier:"customTabbarController") as? UITabBarController else{return}
+        ////        receiveViewController.id = idTextField.text
+        ////        receiveViewController.pw = passwordTextField.text
+        //        receiveViewController.modalPresentationStyle = .fullScreen
+        //        self.present(receiveViewController,animated:true,completion:nil)
+        //        idTextField.text = ""
+        //        passwordTextField.text = ""
+        
     }
     @objc func labelTapped(_ sender:UITapGestureRecognizer){
         guard let vc = self.storyboard?.instantiateViewController(identifier:"signUpViewController") as? SignUpViewController else{return}
         navigationController?.pushViewController(vc, animated:true)
-    
- 
+        
+        
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 extension UITextField{
     func addLeftPadding() {
@@ -69,6 +92,6 @@ extension UITextField{
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
         //tf.leftViewMode = tf.ViewMode.always
-      }
+    }
     
 }
