b638fef383c95904cff925b77e2a5a58220c99b0
seu11ee <yslee2504@naver.com>
2020-05-20 21:12:44 +0900

    3주차

---
+++ 3rdWeek/iOS_FirstWeek_Assignment_2/iOS_FirstWeek_Assignment_2/SignUpViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SignUpViewController.swift
+//  iOS_FirstWeek_Assignment_2
+//
+//  Created by 이예슬 on 4/18/20.
+//  Copyright © 2020 이예슬. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.topItem?.title=""
+        self.navigationController?.navigationBar.tintColor = UIColor(red:7/255,green:59/255,blue:163/255,alpha:1.0)
+        idTextField.layer.cornerRadius = 22
+        pwTextField.layer.cornerRadius = 22
+        nameTextField.layer.cornerRadius=22
+        idTextField.addLeftPadding()
+        pwTextField.addLeftPadding()
+        nameTextField.addLeftPadding()
+        signUpButton.layer.cornerRadius=24
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBAction func signUpData(_ sender: Any) {
+        guard let receiveViewController = self.storyboard?.instantiateViewController(identifier:"logOutViewController") as? LogOutViewController else{return}
+            receiveViewController.id = idTextField.text
+            receiveViewController.pw = pwTextField.text
+            receiveViewController.modalPresentationStyle = .fullScreen
+        self.present(receiveViewController,animated:true){
+            self.navigationController?.popViewController(animated:false)
+        }
+            idTextField.text = ""
+            pwTextField.text = ""
+     
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 3rdWeek/iOS_ThirdWeek_Seminar_Exercise/iOS_ThirdWeek_Seminar_ExerciseTests/iOS_ThirdWeek_Seminar_ExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_ThirdWeek_Seminar_ExerciseTests.swift
+//  iOS_ThirdWeek_Seminar_ExerciseTests
+//
+//  Created by 이예슬 on 5/9/20.
+//  Copyright © 2020 이예슬. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_ThirdWeek_Seminar_Exercise
+
+class iOS_ThirdWeek_Seminar_ExerciseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
